eee43552c0234e20921ecfb38134811e574a91e7
veax <borissowadim@mail.ru>
2019-07-06 22:39:27 +0200

    change about page

--- assets/scripts/scrollTop.js
+++ assets/scripts/scrollTop.js
@@ -1,5 +1,6 @@
 window.onscroll = function() {scrollFunction()};
 
+    // arrow button is visible below 150px vertically
   function scrollFunction() {
       if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
           document.getElementById("back_to_top").style.display = "block";
@@ -12,8 +13,8 @@ window.onscroll = function() {scrollFunction()};
     var scrollStep = -window.scrollY / (scrollDuration / 15),
         scrollInterval = setInterval(function(){
         if ( window.scrollY != 0 ) {
-            window.scrollBy( 0, scrollStep );
+            window.scrollBy( 0, scrollStep );   // scroll vertically in negative dir (to the top)
         }
-        else clearInterval(scrollInterval); 
+        else clearInterval(scrollInterval);     // stop setInterval scrolling
         },15);
   }
--- assets/scripts/toggle.js
+++ assets/scripts/toggle.js
@@ -1,10 +1,9 @@
 // Show an element
 var show = function (elem) {
-
   // Get the natural height of the element
   var getHeight = function () {
     elem.style.display = 'block'; // Make it visible
-    var height = elem.scrollHeight + 'px'; // Get it's height
+    var height = elem.scrollHeight + 'px'; // Get it's height - scrollHeight is only for read
     elem.style.display = ''; //  Hide it again
     return height;
   };
@@ -18,54 +17,43 @@ var show = function (elem) {
     elem.style.height = '';
   }, 350);
   
-  };
+};
   
-  // Hide an element
-  var hide = function (elem) {
-  
+// Hide an element
+var hide = function (elem) {
   // Give the element a height to change from
   elem.style.height = elem.scrollHeight + 'px';
-  
   // Set the height back to 0
   window.setTimeout(function () {
     elem.style.height = '0';
   }, 1);
-  
   // When the transition is complete, hide it
   window.setTimeout(function () {
     elem.classList.remove('is-visible');
   }, 350);
   
-  };
+};
   
-  // Toggle element visibility
-  var toggle = function (elem, timing) {
-  
+// Toggle element visibility
+var toggle = function (elem, timing) {
   // If the element is visible, hide it
   if (elem.classList.contains('is-visible')) {
     hide(elem);
     return;
   }
-  
   // Otherwise, show it
   show(elem);
+};
   
-  };
-  
-  // Listen for click events
-  document.addEventListener('click', function (event) {
-  
+// Listen for click events
+document.addEventListener('click', function (event) {
   // Make sure clicked element is our toggle
   if (!event.target.classList.contains('toggle')) return;
-  
   // Prevent default link behavior
   event.preventDefault();
-  
   // Get the content
   var content = document.querySelector(event.target.hash);
   if (!content) return;
-  
   // Toggle the content
   toggle(content);
-  
-  }, false);
+}, false);
